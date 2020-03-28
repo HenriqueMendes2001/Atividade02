59d73a91e17411b32509b616dbbeecaebb16f5c8
HenriqueMendes2001 <50620893+HenriqueMendes2001@users.noreply.github.com>
2020-03-27 21:52:35 -0300

    Update InserindoNaTabela.sql

--- InserindoNaTabela.sql
+++ InserindoNaTabela.sql
@@ -1,7 +1,7 @@
 USE pais;
 
-INSERT INTO paises (nome, populacao, area) VALUES ('Alemanha', 82800000, 357051.00);
-INSERT INTO paises (nome, populacao, area) VALUES ('Itália', 60317546, 301340.00);
-INSERT INTO paises (nome, populacao, area) VALUES ('Russia', 146745098, 17098246.00);
-INSERT INTO paises (nome, populacao, area) VALUES ('China', 1401812360, 9569961.00);
-INSERT INTO paises (nome, populacao, area) VALUES ('India', 1359933123, 3287263.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('Alemanha', 7454612, 44512884.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('França', 44541522, 6564515.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('Bolivia', 7445525, 4541121.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('Japão', 1452566, 5121151.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('India', 15154511, 152121.00);
